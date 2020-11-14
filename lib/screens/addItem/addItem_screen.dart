c5b6223a2870ed78e4907aa96a4952a38bb24cae
benicamera <66155472+benicamera@users.noreply.github.com>
2020-11-14 19:55:23 +0100

    Suchergebnisliste implementiert.

--- lib/screens/addItem/addItem_screen.dart
+++ lib/screens/addItem/addItem_screen.dart
@@ -3,13 +3,23 @@
 *   @version: 14.11.2020
  */
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:q_shop/constants.dart';
+import 'package:q_shop/models/main_divider_bar.dart';
+import 'package:q_shop/screens/details/details_screen.dart';
 
+import '../../icons.dart';
 import 'components/searchbar.dart';
 
 class AddItemScreen extends StatelessWidget {
-  void refreshResults(String value){
+  var searchResults = new List();
+
+  AddItemScreen() {
+    searchResults.add("Produkt erstellen");
+  }
+
+  void refreshResults(String value) {
     print(value + " ... refreshing...");
   }
 
@@ -23,12 +33,29 @@ class AddItemScreen extends StatelessWidget {
           children: [
             Container(height: MediaQuery.of(context).size.height / 100),
             Center(
-              child: SearchBar(onChanged: (String value){
-                refreshResults(value);
-                return;
-              },
+              child: SearchBar(
+                onChanged: (String value) {
+                  refreshResults(value);
+                  return;
+                },
               ),
             ),
+            Container(height: MediaQuery.of(context).size.height / 60),
+            DividerBar(
+              title: "Suchergebnisse",
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: searchResults.length,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int i) {
+                    return Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: MediaQuery.of(context).size.width / 20,
+                            vertical: MediaQuery.of(context).size.height / 50),
+                        child: SearchResult(item: searchResults[i]));
+                  }),
+            )
           ],
         ));
   }
@@ -75,3 +102,58 @@ class AddItemScreen extends StatelessWidget {
   }
 }
 
+class SearchResult extends StatelessWidget {
+  const SearchResult({
+    Key key,
+    @required this.item,
+  }) : super(key: key);
+
+  final item;
+
+  void toDetailScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DetailsScreen(
+                  itemName: item,
+                  weight: "1 Stk",
+                  itemIcon: apfel,
+                  isProposal: true,
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          (item == "Produkt erstellen")
+              ? print("Produkt erstellen..")
+              : toDetailScreen(context);
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height / 15,
+          width: MediaQuery.of(context).size.width * 0.96,
+          decoration: BoxDecoration(
+            color: kDarkGrey1,
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Center(
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width / 10),
+                  child: Text(
+                    item,
+                    style: TextStyle(
+                        fontSize: 22,
+                        color:
+                            kLightGrey2), //TODO: Icon einfügen und automatic font
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
