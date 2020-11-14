9775a8f858333a71807671dd5df90f88fe701ec4
bernhardgschwind <bernhard.gschwind@gmail.com>
2020-11-14 11:03:54 +0100

    debug

---
+++ lib/screens/shopping/components/shopping_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:q_shop/models/main_createlist_button_big.dart';
+import 'package:q_shop/models/main_shopping_list_view.dart';
+import 'package:q_shop/models/main_divider_bar.dart';
+
+import '../../../constants.dart';
+
+class Shopping_Body extends StatefulWidget {
+  @override
+  _Shopping_BodyState createState() => _Shopping_BodyState();
+}
