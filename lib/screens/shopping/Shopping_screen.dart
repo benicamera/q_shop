8f0574673842e3b1d797df469db5768ce469b9a9
bernhardgschwind <bernhard.gschwind@gmail.com>
2020-11-23 20:03:22 +0100

    debug

--- lib/screens/shopping/Shopping_screen.dart
+++ lib/screens/shopping/Shopping_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:q_shop/constants.dart';
+import 'package:q_shop/icons.dart';
 
 
 class ShoppingScreen extends StatelessWidget {
@@ -7,24 +8,94 @@ class ShoppingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:MyHomePage(),
+    home: MyHomePage(),
     );
   }
 }
- class MyHomePage extends StatelessWidget {
-  List<String> items = List<String>.generate(20, (int counter) => "item#$counter");
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: items.length,
-          itemExtent: 220,
-          itemBuilder: (BuildContext context, int index){
-            return ListTile(
-              title: Text(items[index]),
-            );
-          }
-      ),
-    );
+        body: Center(child: SwipeList()));
   }
- }
+}
+
+class SwipeList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListItemWidget();
+  }
+}
+
+class ListItemWidget extends State<SwipeList> {
+  List items = getDummyList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView.builder(
+
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              key: Key(items[index]),
+              background: Container(
+                alignment: AlignmentDirectional.centerEnd,
+                color: Colors.red,
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+              onDismissed: (direction) {
+                setState(() {
+                  items.removeAt(index);
+                });
+              },
+              direction: DismissDirection.endToStart,
+              child: Card(
+                elevation: 5,
+                child: Container(
+                  height: 225.0,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 100.0,
+                        width: 70.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(5),
+                                topLeft: Radius.circular(5)
+                            ),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                items[index],
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
+  }
+
+  static List getDummyList() {
+    List list = List.generate(10, (i) {
+      return "Item ${i + 1}";
+    });
+    return list;
+  }
+}
